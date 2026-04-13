75ee23f596eb4cad183a366131312dc3bb3912b9
junior <antonioivo.3@gmail.com>
2026-04-13 18:29:03 -0300

    bugfix-docker

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
 const loggerMiddleware = require('./shared/middlewares/loggerMiddleware');
@@ -6,7 +8,9 @@ const authRoutes = require('./modules/auth/routes/auth.routes');
 const errorMiddleware = require('./shared/middlewares/errorMiddleware');
 const rateLimitMiddleware = require('./shared/middlewares/rateLimitMiddleware');
 
-
+app.use(cors({
+  origin: ['*'],
+}));
 app.use(express.json());
 app.use(rateLimitMiddleware);
 app.use(loggerMiddleware);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
-const { port } = require('./config/env');
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
